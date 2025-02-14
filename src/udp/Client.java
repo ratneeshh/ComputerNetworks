bbeafb8ddc35c16ca5280e98977269984c7b2387
ratneeshh <ratnesha.ug23.cs@nitp.ac.in>
2025-02-14 11:19:33 +0530

    udp code added

--- src/udp/Client.java
+++ src/udp/Client.java
@@ -1,4 +1,46 @@
 package udp;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
+
+    public static void main(String[] args) {
+
+        try {
+
+            DatagramSocket socket = new DatagramSocket(); //creating a client socket
+            InetAddress serverAddress = InetAddress.getByName("localhost");  //fetch ip address
+            int serverPort = 9806;
+            System.out.println("Client Started");
+
+            //read from keyboard
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a string: ");
+            String str = userInput.readLine();
+
+            //send to server
+            byte[] sendBuffer = str.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, serverPort);
+            socket.send(sendPacket);
+            System.out.println("Message sent to Server");
+
+            //receive from server
+            byte[] recieveFromServer = new byte[1024];
+            while(true){
+                DatagramPacket receivePacket = new DatagramPacket(recieveFromServer, recieveFromServer.length);
+                socket.receive(receivePacket);
+                String receivedData = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Message Received from Server");
+                System.out.println("Length of String: "+receivedData.length());
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/udp/Server.java
+++ src/udp/Server.java
@@ -1,4 +1,41 @@
 package udp;
 
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Server {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("UDP Server is running");
+            System.out.println("Waiting for Clients...");
+            DatagramSocket socket = new DatagramSocket(9806);
+
+            byte[] recieveBuffer = new byte[1024]; //datapackets is sent in bytes, so to receive it
+
+            //receive from client
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(recieveBuffer, recieveBuffer.length);
+                socket.receive(receivePacket);
+                String receivedData = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Message Received from client");
+
+                //send back to client
+                String responseData = receivedData;
+                byte[] responseDataBytes = responseData.getBytes();
+                DatagramPacket responsePacket = new DatagramPacket(responseDataBytes, responseDataBytes.length, receivePacket.getAddress(), receivePacket.getPort());
+                socket.send(responsePacket);
+                System.out.println("Response sent to client");
+            }
+        }
+
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+    }
 }
